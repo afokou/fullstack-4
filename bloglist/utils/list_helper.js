a29c9615dc03627a88571d9a517336aa475e9df8
Angeliki Fokou <a@fokou.gr>
2024-12-26 17:51:37 +0200

    Exercise up to 4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  const favoriteBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, blogs[0]);
+  if (!favoriteBlog) {
+    return {};
+  }
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  };
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
