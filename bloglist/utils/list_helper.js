258391ea13edecf679c4665ee0a17d1bbf3c6e9f
Angeliki Fokou <a@fokou.gr>
2024-12-26 17:58:44 +0200

    Exercise up to 4.7

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -18,8 +18,42 @@ const favoriteBlog = (blogs) => {
   };
 }
 
+const mostBlogs = (blogs) => {
+  const authors = blogs.reduce((authors, blog) => {
+    if (!authors[blog.author]) {
+      authors[blog.author] = 1;
+    } else {
+      authors[blog.author]++;
+    }
+    return authors;
+  }, {});
+  const authorWithMostBlogs = Object.keys(authors).reduce((max, author) => authors[max] > authors[author] ? max : author, Object.keys(authors)[0]);
+  return {
+    title: authorWithMostBlogs,
+    blogs: authors[authorWithMostBlogs]
+  }
+}
+
+const mostLikes = (blogs) => {
+  const authors = blogs.reduce((authors, blog) => {
+    if (!authors[blog.author]) {
+      authors[blog.author] = blog.likes;
+    } else {
+      authors[blog.author] += blog.likes;
+    }
+    return authors;
+  }, {});
+  const authorWithMostLikes = Object.keys(authors).reduce((max, author) => authors[max] > authors[author] ? max : author, Object.keys(authors)[0]);
+  return {
+    title: authorWithMostLikes,
+    likes: authors[authorWithMostLikes]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
