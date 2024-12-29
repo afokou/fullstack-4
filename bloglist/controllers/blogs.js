55af99c0133e177abafd4816a6f568350b2d7f40
Angeliki Fokou <a@fokou.gr>
2024-12-29 19:09:52 +0200

    Fix mising import for comment

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const Comment = require('../models/comment')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('author', { username: 1, name: 1 })
